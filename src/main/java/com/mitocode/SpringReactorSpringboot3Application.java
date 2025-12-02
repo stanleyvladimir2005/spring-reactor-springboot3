6217c77e18f7429fc561c822a4108ee1b6aeeef5
Vladimir Stanley Alvarez Alvrado <stanleyvladimir2005@gmail.com>
2025-12-02 07:19:20 -0600

    Actualizado a:  1-Spring boot 4.0.0  2-Gradle 9.2.1  3-Java 25  4-Pendiente de revisar ya que hay clases incompatibles

--- src/main/java/com/mitocode/SpringReactorSpringboot3Application.java
+++ src/main/java/com/mitocode/SpringReactorSpringboot3Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringReactorSpringboot3Application {
 
-    public static void main(String[] args) {
+   void main(String[] args) {
         SpringApplication.run(SpringReactorSpringboot3Application.class, args);
     }
 }
